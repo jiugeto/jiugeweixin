19603f7c644850d03257dad61baeec7a2472c02f
root <946493655@qq.com>
2018-04-04 15:00:27 +0800

    对接微信包

--- config/jiugewx.php
+++ config/jiugewx.php
@@ -3,8 +3,11 @@
  * jiuge对接微信的配置文件
  */
 return array(
-    'domain' => '', //网站域名
-    'wechat_token' => '', //微信对接参数
+    'domain' => env('DOMAIN'), //网站域名
+    'wechat_appid' => 'wxe7259d761479c143', //微信对接参数AppId
+    'wechat_appsecret' => '1091fb5f138f95caa054f201484b49bb', //微信对接参数AppSecret
+    'wechat_token' => 'hzkunshi', //微信对接参数TOKEN
+    'wechat_token' => 'D6p3b27cee6EWTkuv0OZ69zEbdSbJCYhBeybvjY1O2q', //微信对接参数EncodingAESKey
 //    'wiki' => array(
 //        'name' => 'wiki',
 //        'content' => 'Welcome to the Amaze UI wiki!',
